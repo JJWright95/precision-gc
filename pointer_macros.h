547a38d5bbc228999ecaca0c8d9dacd68448f3e3
Jonathan Wright <user@crunchie-1>
2017-01-12 21:51:40 +0000

    Updated pointer macros to integrate Stephen's code

--- pointer_macros.h
+++ pointer_macros.h
@@ -4,33 +4,17 @@
 /*
 gc_malloc memory block layout:
 
-+-----------------------------+-----------+-----------+
-|          User data          | *previous |   *next   |
-+-----------------------------+-----------+-----------+
-                              ^
-                        INSERT_ADDRESS
-*/
-
-#define POINTER_SIZE sizeof(void *)
-#define INSERT_ADDRESS(BLOCK_ADDRESS) ((void *) ((char *) BLOCK_ADDRESS+malloc_usable_size(BLOCK_ADDRESS)-2*POINTER_SIZE))
-#define PREVIOUS_POINTER(BLOCK_ADDRESS) (*((void **) ((char *) BLOCK_ADDRESS+malloc_usable_size(BLOCK_ADDRESS)-2*POINTER_SIZE)))
-#define NEXT_POINTER(BLOCK_ADDRESS) (*((void **) ((char *) BLOCK_ADDRESS+malloc_usable_size(BLOCK_ADDRESS)-POINTER_SIZE)))
-
-/* What macros will look like when using Stephen's insert
-
-#define INSERT_ADDRESS(BLOCK_ADDRESS) ((void *) ((char *) __liballocs_insert_for_chunk_and_usable_size(BLOCK_ADDRESS, malloc_usable_size(BLOCK_ADDRESS)) -2*POINTER_SIZE))
-#define PREVIOUS_POINTER(BLOCK_ADDRESS) (*((void **) ((char *) __liballocs_insert_for_chunk_and_usable_size(BLOCK_ADDRESS, malloc_usable_size(BLOCK_ADDRESS)) -2*POINTER_SIZE)))
-#define NEXT_POINTER(BLOCK_ADDRESS) (*((void **) ((char *) __liballocs_insert_for_chunk_and_usable_size(BLOCK_ADDRESS, malloc_usable_size(BLOCK_ADDRESS)) -POINTER_SIZE)))
-*/
-
-/*
-gc_malloc memory block layout:
-
 +-----------------------------+-----------+-----------+------------------+
 |          User data          | *previous |   *next   | Stephen's Insert |
 +-----------------------------+-----------+-----------+------------------+
                               ^
                         INSERT_ADDRESS
 */
+
+#define POINTER_SIZE sizeof(void *)
+#define INSERT_ADDRESS(BLOCK_ADDRESS) ((void *) ((char *) BLOCK_ADDRESS+malloc_usable_size(BLOCK_ADDRESS)-sizeof(struct insert)-2*POINTER_SIZE))
+#define PREVIOUS_POINTER(BLOCK_ADDRESS) (*((void **) ((char *) BLOCK_ADDRESS+malloc_usable_size(BLOCK_ADDRESS)-sizeof(struct insert)- 2*POINTER_SIZE)))
+#define NEXT_POINTER(BLOCK_ADDRESS) (*((void **) ((char *) BLOCK_ADDRESS+malloc_usable_size(BLOCK_ADDRESS)-sizeof(struct insert)-POINTER_SIZE)))
+
 
 #endif
